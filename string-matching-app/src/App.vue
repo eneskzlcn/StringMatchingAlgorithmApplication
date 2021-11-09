ef50d1ed33aa566b0ae90118fbe418ac1426f809
Enes Kızılcin <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-11-09 22:54:46 +0300

    vue project initialized to be use as frontend technology

---
+++ string-matching-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <StringMatching msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import StringMatching from './components/StringMatching.vue'
+
+export default {
+  name: 'App',
+  components: {
+    StringMatching
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
